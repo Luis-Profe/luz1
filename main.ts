289f24422a517ed55536194371bbe4463753437b
Luis-Profe <98078028+Luis-Profe@users.noreply.github.com>
2022-04-19 20:16:00 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
-basic.forever(function () {
-	
+input.onGesture(Gesture.ScreenUp, function () {
+    moneda = randint(0, 1)
+    if (moneda == 0) {
+        basic.showIcon(IconNames.Happy)
+    } else {
+        basic.showIcon(IconNames.No)
+        basic.clearScreen()
+        basic.pause(1000)
+    }
 })
+let moneda = 0
+moneda = 0
